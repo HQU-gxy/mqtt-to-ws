68c195b715e0a688db1c327ffe66bda75b4c2a11
Crosstyan <cf595833532@gmail.com>
2021-12-15 23:46:10 +0800

    fix: makefile os detect

--- docs/docs.go
+++ docs/docs.go
@@ -20,7 +20,7 @@ var doc = `{
         "termsOfService": "http://swagger.io/terms/",
         "contact": {
             "name": "Crosstyan",
-            "url": "github.com/crosstyan/mqtt-to-ws/",
+            "url": "https://github.com/crosstyan/mqtt-to-ws/",
             "email": "crosstyan@outlook.com"
         },
         "license": {
--- makefile
+++ makefile
@@ -1,5 +1,12 @@
 .PHONY: all build clean run check cover lint docker help
+
+# https://stackoverflow.com/questions/4058840/makefile-that-distinguishes-between-windows-and-unix-like-systems
+ifeq ($(OS),Windows_NT)
+BIN_FILE=bin.exe
+else
 BIN_FILE=bin
+endif
+
 all: check build
 build:
 	@swag init
